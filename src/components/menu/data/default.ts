43225fd07e5ad0370efd839b91fbaae4f934fb97
Pol Gubau Amores <gubaupol@gmail.com>
2025-02-08 20:36:08 +0100

    feat(menu): ✨ add locale and date to default menu item

--- src/components/menu/data/default.ts
+++ src/components/menu/data/default.ts
@@ -3,8 +3,8 @@ import type { MenuItem } from "../helpers/grid";
 export const defaultItems: MenuItem[] = [
 	{
 		image: "https://picsum.photos/900/900?grayscale",
-		link: "https://google.com/",
 		title: "",
-		description: "",
+		locale: "en-US",
+		date: new Date(),
 	},
 ];
